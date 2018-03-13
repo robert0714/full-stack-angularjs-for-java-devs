c46e74f4e9e6485e9720959a8c004238a015b545
robert.lee <robert0714@gmail.com>
2018-03-13 15:40:20 +0800

    adjust for compile failure

--- chapter-3/UserRegistrationSystem/src/main/java/com/apress/ravi/chapter2/UserRegistrationConfiguration.java
+++ chapter-3/UserRegistrationSystem/src/main/java/com/apress/ravi/chapter2/UserRegistrationConfiguration.java
@@ -1,14 +1,17 @@
 package com.apress.ravi.chapter2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 public class UserRegistrationConfiguration {
 
 	@Bean(name = "messageSource")
-	public ReloadableResourceBundleMessageSource messageSource() {
+	@Autowired
+	public ReloadableResourceBundleMessageSource messageSource( ) {
 		ReloadableResourceBundleMessageSource messageBundle = new ReloadableResourceBundleMessageSource();
 		messageBundle.setBasename("classpath:messages/messages");
 		messageBundle.setDefaultEncoding("UTF-8");
